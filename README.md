0b5d8501eef0dd381bbefe46805a4f7d51080fc4
Lilien Auger <lauger@1A3.42angouleme.fr>
2023-11-29 17:42:48 +0100

    first add

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# 42-get_next_line
+# get_next_line
+
+Ce projet consiste en l'implémentation de la fonction `get_next_line`, une fonction qui lit une ligne d'un fichier descripteur de fichier ou d'un flux d'entrée standard.
+
+## Fonctionnement
+
+La fonction `get_next_line` permet de lire une ligne (jusqu'à un caractère de nouvelle ligne) à partir d'un fichier descripteur de fichier (file descriptor) et de stocker cette ligne dans une chaîne de caractères allouée dynamiquement. À chaque appel de la fonction, la ligne suivante du fichier est lue jusqu'à ce qu'une nouvelle ligne soit trouvée ou que la fin du fichier soit atteinte.
+
+## Instalation
+
+1. Clonez ce référentiel sur votre machine locale.
+	```bash
+	https://github.com/Lilien86/42-get_next_line.git
+	```
+2. Compilez le programme avec les fichiers source de la fonction (et vortre propre main.c)
+	```bash
+	cc get_next_line.c get_next_line.h get_next_line_utils.c main.c
+	```
+## Utilisation
+
+1. Incluez le fichier d'en-tête get_next_line.h et fcntl.h
+	```c
+	#include "get_next_line.h"
+	#include "<fcntl.h>"
+	```
+2. Utilisez la fonction get_next_line
+	```c
+	int main() {
+    int fd = open("exemple.txt", O_RDONLY);
+    char *line;
+
+    while (get_next_line(fd, &line) > 0) {
+        // Traitement de la ligne lue
+        printf("Ligne lue : %s\n", line);
+        free(line);
+    }
+
+    close(fd);
+    return 0;
+	}
+	```
+
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lauger <lauger@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/21 14:40:21 by lauger            #+#    #+#             */
+/*   Updated: 2023/11/23 13:27:23 by lauger           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int	check_str_char(char *line, char *buffer)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (line[i] != '\n' && line[i])
+		i++;
+	if (line[i] == '\n')
+	{
+		i++;
+		while (line[i])
+		{
+			buffer[j] = line[i];
+			j++;
+			i++;
+		}
+		buffer[j] = '\0';
+		line[i - j] = 0;
+		return (1);
+	}
+	return (0);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t			cnt;
+	unsigned char	*temp;
+	unsigned char	c;
+
+	temp = (unsigned char *)s;
+	c = '\0';
+	cnt = 0;
+	while (cnt < n)
+	{
+		temp[cnt] = c;
+		cnt++;
+	}
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	if (count == 0 || size == 0)
+	{
+		ptr = malloc(1);
+		if (!ptr)
+			return (NULL);
+		ft_bzero(ptr, 1);
+		return (ptr);
+	}
+	if (count * size / size != count)
+		return (0);
+	ptr = malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, count * size);
+	return (ptr);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	if (s1 == 0 || s2 == 0)
+		return (NULL);
+	str = malloc(sizeof(*s1) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		str[j++] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i])
+	{
+		str[j++] = s2[i];
+		i++;
+	}
+	free(s1);
+	str[j] = 0;
+	return (str);
+}
